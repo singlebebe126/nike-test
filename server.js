a46a4477334f93422a3d03e4eab4567c5b8c930c
unknown <singlebebe126@gmail.com>
2023-08-03 13:21:52 +0700

    first attempt

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "/index.html"))
+})
+
+app.listen(8080, () => {
+    console.log("Server listening to port 8080");
+})
